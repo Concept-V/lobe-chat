a0f8e8a8e94589b2b3ad0d963a2e07c4530fcfe6
Nick Pang <nick95927@gmail.com>
2025-02-12 21:09:08 +1000

    Updated branding

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'Vi Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'Vi 5.0 (Preview)';
+export const BRANDING_LOGO_URL = 'https://api.conceptv.com.au/api/logo';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'ConceptV';
 
 export const BRANDING_URL = {
   help: undefined,
